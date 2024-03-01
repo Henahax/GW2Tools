427b6d2e00f38f5e0282c5e146df279bc5fdb329
Henahax <mail@henahax.de>
2024-03-01 02:29:01 +0100

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,32 +9,26 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">GW2 Tools</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
+				<a href="/" class="btn hover:variant-soft-primary">
+					<span>
+						<i class="fa-solid fa-house"></i>
+					</span>
+					<span>Home</span>
 				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
+				<a href="/reset" class="btn hover:variant-soft-primary">
+					<span>
+						<i class="fa-solid fa-arrows-rotate"></i>
+					</span>
+					<span>Reset</span>
 				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+				<a href="/magicfind" class="btn hover:variant-soft-primary">
+					<span>
+						<i class="fa-solid fa-clover"></i>
+					</span>
+					<span>MagicFind</span>
 				</a>
 			</svelte:fragment>
 		</AppBar>
--- src/routes/magicfind/+page.svelte
+++ src/routes/magicfind/+page.svelte
@@ -1,1 +1,31 @@
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
 <h2>MagicFind</h2>
+
+<div class="table-container">
+	<!-- Native Table Element -->
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Position</th>
+				<th>Name</th>
+				<th>Symbol</th>
+				<th>Weight</th>
+			</tr>
+		</thead>
+		<tbody>
+				<tr>
+					<td>test</td>
+					<td>test2</td>
+					<td>test3</td>
+					<td>test4</td>
+				</tr>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th colspan="3">Calculated Total Weight</th>
+				<td>xtest</td>
+			</tr>
+		</tfoot>
+	</table>
+</div>
+</div>
