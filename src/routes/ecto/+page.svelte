4142c1ea1787b5f1cf58e0ca4545c49e03da3267
Henahax <mail@henahax.de>
2024-08-11 01:35:17 +0200

    ecto init

---
+++ src/routes/ecto/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Title from '$lib/Title.svelte';
+	import salvagekits from './salvage.json';
+</script>
+
+<svelte:head>
+	<title>GW2Tools: Ecto Salvage</title>
+</svelte:head>
+
+<Title title="Ecto Salvage" subtitle="Is salvaging Ectos to Dust profitable?"></Title>
+
+<div class="mx-auto grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+	{#each salvagekits as salvagekit}
+		<div class="card bg-base-100 flex flex-col gap-2 p-4 shadow">
+			<div class="flex flex-row items-center justify-start gap-2">
+				<img class="size-8 rounded-md" src={salvagekit.icon} alt={salvagekit.name} />
+				<div class="text-lg">{salvagekit.name}</div>
+			</div>
+			<slot />
+		</div>
+	{/each}
+</div>
