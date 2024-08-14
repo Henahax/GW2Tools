74a7acc3d50ffb4ceb0285da3dceccdb2bd39385
Tobias <mail@henahax.de>
2024-08-14 15:36:30 +0200

    Update +page.svelte

--- src/routes/ecto/+page.svelte
+++ src/routes/ecto/+page.svelte
@@ -7,9 +7,14 @@
 	<title>GW2Tools: Ecto Salvage</title>
 </svelte:head>
 
-<Title title="Ecto Salvage" subtitle="Is salvaging Ectos to Dust profitable?"></Title>
+<Title title="Ecto Salvage" subtitle="Is salvaging Ectos to Dust profitable?">
+	<button class="btn btn-primary max-md:btn-square">
+		<i class="fa-solid fa-question"></i>
+		<div class="max-md:hidden">Info</div>
+	</button>
+</Title>
 
-<div class="mx-auto grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+<div class="mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-4">
 	{#each salvagekits as salvagekit}
 		<div class="card bg-base-100 flex flex-col gap-2 p-4 shadow">
 			<div class="flex flex-row items-center justify-start gap-2">
