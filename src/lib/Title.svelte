ac09d0c61384a06fbc8f99827b918ff0b9eba2bd
Tobias <mail@henahax.de>
2024-08-14 15:24:20 +0200

    Update Title.svelte

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -3,7 +3,7 @@
 	export let subtitle;
 </script>
 
-<div class="flex w-full flex-row justify-between p-4 items-center gap-4">
+<div class="flex w-full flex-row justify-between p-4 items-center gap-4 max-w-screen-2xl mx-auto">
 	<div class="flex flex-col">
 		<h2 class="text-2xl font-bold">{title}</h2>
 		<span class="text-sm">{subtitle}</span>
