d23f0c1b235e6c7b5f4cb904fe2fcde54f8ece53
Henahax <mail@henahax.de>
2024-11-05 00:56:21 +0100

    cat task

---
+++ src/lib/components/Category.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+    import Task from './Task.svelte';
+</script>
+
+<Collapsible.Root open class="w-fit bg-green-900">
+	<Collapsible.Trigger asChild let:builder>
+		<Button builders={[builder]} variant="ghost" size="sm" class="w-full">
+			<slot name="title" />
+		</Button>
+	</Collapsible.Trigger>
+	<Collapsible.Content>
+		<ul>
+			<Task></Task>
+            <Task></Task>
+		</ul>
+	</Collapsible.Content>
+</Collapsible.Root>
---
+++ src/routes/reset/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Category from "$lib/components/Category.svelte";
+</script>
+
+<Category>
+    <h2 slot="title">Test</h2>
+</Category>
