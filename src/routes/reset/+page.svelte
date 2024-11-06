20b1a38ea65bbe8fce3955d4cc9a080edde7fa23
Henahax <mail@henahax.de>
2024-11-06 22:49:21 +0100

    stuff

--- src/routes/reset/+page.svelte
+++ src/routes/reset/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	import CategoryElement from '$lib/components/CategoryElement.svelte';
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Checkbox } from '$lib/components/ui/checkbox';
+
+	import EventTimer from '$lib/components/EventTimer.svelte';
 
 	import type { Interval, Category, Task } from '$lib/types/types';
 
@@ -70,21 +74,83 @@
 	});
 </script>
 
-<div class="flex flex-col gap-8 p-4 sm:flex-row">
+<div class="flex flex-col gap-4 p-4 sm:flex-row mx-auto">
 	{#each data as interval}
-		<div class="flex flex-col {interval.id === 'weekly' ? '' : 'grow'}">
-			<h3 class="p-2">{interval.tasks}:</h3>
-			<div
-				class="space-y-4 {interval.id === 'weekly'
-					? 'columns-1'
-					: 'columns-1 md:columns-2 lg:columns-3 xl:columns-4'}"
-			>
-				{#each interval.categories as category}
-					<CategoryElement category={category}>
-						<h2 slot="title">{category.name}</h2>
-					</CategoryElement>
-				{/each}
+		{#if interval.categories.reduce((sum, category) => sum + category.tasks.filter((task) => task.display).length, 0) > 0}
+			<div class="flex flex-col gap-2">
+				<h2 class="px-2 text-lg font-bold">
+					{#if interval && interval.categories}
+						{interval.tasks}
+						({interval.categories.reduce(
+							(sum, category) =>
+								sum + category.tasks.filter((task) => task.checked && task.display).length,
+							0
+						)}/{interval.categories.reduce(
+							(sum, category) => sum + category.tasks.filter((task) => task.display).length,
+							0
+						)}):
+					{/if}
+				</h2>
+
+				<div
+					class="columns-1 gap-2 space-y-2 {interval.id === 'daily'
+						? 'lg:columns-2 xl:columns-3'
+						: ''}"
+				>
+					{#each interval.categories as category}
+						<Collapsible.Root open class="w-full break-inside-avoid rounded-xl border">
+							<Collapsible.Trigger asChild let:builder>
+								<Button builders={[builder]} variant="ghost" size="sm" class="w-full">
+									<h3>{category.name}</h3>
+								</Button>
+							</Collapsible.Trigger>
+							<Collapsible.Content class="p-2">
+								<ul class="flex flex-col justify-center gap-2 px-2">
+									{#each category.tasks as task}
+										<li class="flex flex-row items-center gap-2">
+											<label class="flex grow flex-row items-center gap-4">
+												<Checkbox class="scale-150" />
+												<img class="size-8" src={task.icon} alt={task.name} />
+												<div class="flex flex-col">
+													<div class="text-sm font-semibold">{task.name}</div>
+													{#if task.location}
+														<div class="text-xs opacity-70">
+															<i class="fa-solid fa-location-dot"></i>
+															{task.location}
+														</div>
+													{/if}
+													{#if task.description}
+														<div class="text-xs opacity-70">{task.description}</div>
+													{/if}
+												</div>
+											</label>
+											{#if !task.checked}
+												<div class="flex flex-col justify-center">
+													<div class="flex flex-row justify-end gap-1 text-sm">
+														<a
+															class="hover:opacity-50"
+															href={task.link}
+															title="more info"
+															aria-label="more info"
+														>
+															<i class="fa-regular fa-circle-question"></i>
+														</a>
+													</div>
+													{#if task.timer}
+														<div class="flex flex-col justify-end text-right text-xs">
+															<EventTimer {task} />
+														</div>
+													{/if}
+												</div>
+											{/if}
+										</li>
+									{/each}
+								</ul>
+							</Collapsible.Content>
+						</Collapsible.Root>
+					{/each}
+				</div>
 			</div>
-		</div>
+		{/if}
 	{/each}
 </div>
